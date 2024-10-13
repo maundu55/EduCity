a4e73b90e1e80e8426e7104a218cab95bd72744a
maundu55 <marymaundu.mm@gmail.com>
2024-10-13 22:12:28 +0300

    add more About section css

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import './About.css'
+import student from '../../assets/student.jpg'
+import play_circle from '../../assets/play_circle.png'
 
 const About = () => {
   return (
     <div className='about' >
-      
+      <div className='about-left'>
+        <img src={student} alt="" className='about-img' />
+        <img src={play_circle} alt="" className='play-icon' />
+      </div>
+      <div className='about-right'>
+        <h3>ABOUT UNIVERSITY</h3>
+        <h2>Nurturing Tomorrow's Leaders Today</h2>
+        <p>Embark on a transformative educational journey with our university's comprehensive education programs. Our cutting-edge curriculum is desgned to empower students with the knowledge, skills and experiences needed to excel in the dynamic field of education</p>
+        <p>With a focus on innovation, hands-on learning, and personalized mentorship, our programs prepare aspiring educators to make a meaningful impact in classrooms, schools, and communities.</p>
+        <p>Whether you aspire to become a teacher, administrator, counselor, or educational leader, our diverse range of programs offers the perfect pathway to achieve your goals and unlock your full potential in shaping the future of education</p>
+      </div>
     </div>
   )
 }
