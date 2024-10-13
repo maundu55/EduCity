ed9cddd758f6355b86b8af00c7915ba616ebc5af
maundu55 <marymaundu.mm@gmail.com>
2024-10-13 20:25:53 +0300

    create the  about section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./Components/Navbar/Navbar"
 import Hero from "./Components/Hero/Hero"
 import Programs from "./Components/Programs/Programs"
 import Title from "./Components/Title/Title"
+import About from "./Components/About/About"
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
    <div className="container">
     <Title subTitle ='OUR PROGRAM' title ='What We Offer' />
    <Programs />
+   <About />
    </div>
    
     </>
