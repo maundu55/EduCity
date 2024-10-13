510611d0cd6ac65cf2b3a1244c0af40b930be4d9
maundu55 <marymaundu.mm@gmail.com>
2024-10-13 22:26:05 +0300

    add sticky navbar using usestate and useeffect dynamically

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 
 
 const Navbar = () => {
+  const [sticky, setSticky] =useState(false);
+
+  useEffect(()=>{
+    window.addEventListener('scroll', ()=>{
+      window.scrollY > 50 ? setSticky(true) : setSticky(false)
+    });
+  },[])
+
   return (
-    <nav className='container'> 
+    <nav className={`container ${sticky? 'dark-nav' : ''}`}> 
       {/* <img src="" alt="" className="logo"/> */}
       <h2 className='logo'>EduCity</h2>
       <ul>
